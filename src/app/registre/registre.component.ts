94c2a218a6ca0fe23b6703e6327f8a6af8911b1f
andreyB <boec.n1@mail.ru>
2020-09-13 01:21:17 +0300

    table creating 01:21

--- src/app/registre/registre.component.ts
+++ src/app/registre/registre.component.ts
@@ -26,19 +26,6 @@ interface GOFL {
   viewValue: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-registre',
   templateUrl: './registre.component.html',
@@ -53,7 +40,9 @@ export class RegistreComponent implements OnInit, AfterViewInit {
   ];
   typeContract: TypeContract[] = [
     {value: 'steak-0', viewValue: 'Договор'},
-    {value: 'pizza-1', viewValue: 'Приговор'}
+    {value: 'pizza-1', viewValue: 'Приговор'},
+    {value: 'steak-0', viewValue: 'Депозит'},
+    {value: 'steak-0', viewValue: 'Паразит'}
   ];
   goFl: GOFL[] = [
     {value: 'steak-0', viewValue: 'ГО'},
@@ -61,7 +50,6 @@ export class RegistreComponent implements OnInit, AfterViewInit {
   ];
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   displayedColumn = {Weight: 'weight', Symbol: 'symbol'};
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
   fields = Object.keys(this.displayedColumn);
 
 
@@ -83,7 +71,7 @@ export class RegistreComponent implements OnInit, AfterViewInit {
                   currency: 'RUR',
                   amount: '10000',
                   bic_of_bank: '42202837',
-                  AccountBalanceCurrency: 'RUR',
+                  AccountBalanceCurrency: 'BEL',
                   name_of_bank: 'Банк ВТБ  (ПАО)',
                   id_bank_account: '1234567890',
                   comment: null,
@@ -93,7 +81,7 @@ export class RegistreComponent implements OnInit, AfterViewInit {
                   kpp: '17499236',
                   name: 'АО Организация Росатома',
                   is_filial: 'ГО',
-                  type_of_contract: 'Депозит',
+                  type_of_contract: 'Договор',
                   date_of_signing: '2020-01-01',
                   date_start: '2020-01-02',
                   date_end: '2020-02-02',
@@ -111,7 +99,6 @@ export class RegistreComponent implements OnInit, AfterViewInit {
   ngOnInit() {
   }
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
   }
 
   public importXlsx(event): void {
