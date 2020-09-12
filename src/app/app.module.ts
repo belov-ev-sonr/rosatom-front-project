83784076a05f842218ba79c58c1cd7f05591c84f
andreyB <boec.n1@mail.ru>
2020-09-12 16:55:18 +0300

    uahahahahaha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistreComponent } from './registre/registre.component';
+import {MatDatepickerModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatSortModule, MatTableModule} from '@angular/material';
+import { DatapickerComponent } from './datapicker/datapicker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistreComponent,
+    DatapickerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
